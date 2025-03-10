cec35fc082aeb88c8f31185eb8aff5218e16ec91
Garry Hughes <gazm81@yahoo.com>
2025-03-10 17:00:57 +1100

    some pester tests

--- tests/BlindCalculator.Tests.ps1
+++ tests/BlindCalculator.Tests.ps1
@@ -2,14 +2,42 @@ BeforeAll {
     Import-Module "$PSScriptRoot/../src/modules/BlindCalculator/BlindCalculator.psm1"
 }
 
-Describe "Calculate-BlindStructure" {
-    It "Should calculate correct number of rounds" {
-        $result = Calculate-BlindStructure -numPlayers 9 -roundLengthMinutes 20 -startingSmallBlind 25 -startingBigBlind 50 -startingChips 1500
-        $result.Count | Should -Be 15
+Describe "Get-RoundedpBlind" {
+    It "should return the correct rounded small blind" {
+        $result = Get-RoundedpBlind -SmallBlind 1000 -ProposedSmallBlind 1500
+        $result | Should -Be 2000
+    }
+}
+
+Describe "Get-PokerBlindStructure" {
+    It "should return the correct blind structure" {
+        $params = @{
+            NumberOfPlayers = 5
+            StartingSmallBlind = 1000
+            StartingStack = 100000
+            BlindDurationMinutes = 20
+            TournamentDurationHours = 3
+            ChipTypes = @(1000, 5000, 25000, 50000)
+            GameStartTime = (Get-Date)
+        }
+        $result = Get-PokerBlindStructure @params
+        $result.Count | Should -BeGreaterThan 0
+        $result[0].SmallBlind | Should -Be 1000
+        $result[0].BigBlind | Should -Be 2000
     }
 
-    It "Should have increasing blind levels" {
-        $result = Calculate-BlindStructure -numPlayers 9 -roundLengthMinutes 20 -startingSmallBlind 25 -startingBigBlind 50 -startingChips 1500
-        $result[1].BigBlind | Should -BeGreaterThan $result[0].BigBlind
+    It "should handle rebuys correctly" {
+        $params = @{
+            NumberOfPlayers = 5
+            StartingSmallBlind = 1000
+            StartingStack = 100000
+            Rebuys = 2
+            BlindDurationMinutes = 20
+            TournamentDurationHours = 3
+            ChipTypes = @(1000, 5000, 25000, 50000)
+            GameStartTime = (Get-Date)
+        }
+        $result = Get-PokerBlindStructure @params
+        $result.Count | Should -BeGreaterThan 0
     }
 }
